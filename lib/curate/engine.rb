76fb3762722681516799e1e3f7479fb6022538af
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-05 15:49:11 -0500

    Updating generator to use rspec

--- lib/curate/engine.rb
+++ lib/curate/engine.rb
@@ -1,6 +1,11 @@
 module Curate
   class Engine < ::Rails::Engine
-    
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
     def paths
       @curate_paths ||= begin
         super_paths = super
